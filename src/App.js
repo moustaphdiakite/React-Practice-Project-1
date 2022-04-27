5b90dcebaf8560d8404c9ce27d380bdbf63c3832
Moustaph Diakite <63529813+moustaphdiakite@users.noreply.github.com>
2022-04-27 12:22:53 -0300

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -13,10 +13,10 @@ function App() {
 		});
 	}
 	return (
-		<div>
+		<React.Fragment>
 			<AddUser onAddUser={addUserHandler}></AddUser>
 			<UsersList users={usersList}></UsersList>
-		</div>
+		</React.Fragment>
 	);
 }
 
